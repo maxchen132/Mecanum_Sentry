249e8837eb0d539b9b8276f9b0f8f6dad9c1f730
jia-xie <103150184+jia-xie@users.noreply.github.com>
2023-10-19 20:45:55 -0400

    random shooting

--- HigherLevelApps/HigherLevelApps.c/Shooting_Control.c
+++ HigherLevelApps/HigherLevelApps.c/Shooting_Control.c
@@ -10,9 +10,11 @@
  */
 #include "Shooting_Control.h"
 
+uint8_t niuSystick = 0;
+
 Shooting_t Shooting;
 Ramp_Calc_t Fric_Wheel_Ramp;
-
+uint16_t counter_1 = 0;
 void Shooting_Init(void);
 void Turn_Friction_Wheel_On(void);
 void Turn_Friction_Wheel_Off(void);
@@ -27,6 +29,7 @@ void Shooting_Init(void)
 {
 	Shooting.Fric_Wheel.Turned_On = 0;
 	Ramp_Calc_Func.Clear_Ramp(&Fric_Wheel_Ramp);
+	srand(1);
 }
 
 void Trigger_Get_Data(Shooting_t *Shooting)
@@ -36,15 +39,17 @@ void Trigger_Get_Data(Shooting_t *Shooting)
 		// Dial wheel up for single fire
 		if (DR16_Export_Data.Remote_Control.Dial_Wheel < -50)
 		{
-			Shooting->Type.Right_Burst_Flag = 0;
-			Shooting->Type.Left_Burst_Flag = 1;
+			//			Shooting->Type.Right_Burst_Flag = 0;
+			//			Shooting->Type.Left_Burst_Flag = 1;
+			Shooting->Type.Right_Single_Fire_Flag = 1;
 		}
 
 		// Dial wheel down for burst
 		else if (DR16_Export_Data.Remote_Control.Dial_Wheel > 50)
 		{
-			Shooting->Type.Right_Burst_Flag = 1;
-			Shooting->Type.Left_Burst_Flag = 0;
+			Shooting->Type.Right_Single_Fire_Flag = 1;
+			//			Shooting->Type.Right_Burst_Flag = 1;
+			//			Shooting->Type.Left_Burst_Flag = 0;
 		}
 
 		// Return to middle for reset
@@ -72,28 +77,41 @@ void Shooting_Processing(Shooting_t *Shooting)
 	{
 		if (State_Machine.Control_Source == Remote_Control)
 		{
-			if (Shooting->Type.Left_Burst_Flag)
+			if (Shooting->Type.Right_Single_Fire_Flag && (Shooting->Type.Right_Single_Fired_Flag == 0))
 			{
-				Shooting->Trigger.Target_Speed = LEFT_TRIGGER_DIRECTION * 5000; // The multiplying constant determines the frequency of bursting
-				M2006_Trigger[0].Output_Current = PID_Func.Positional_PID(&Trigger_Speed_PID, Shooting->Trigger.Target_Speed, M2006_Trigger[0].Actual_Speed);
+				PID_Func.Clear_PID_Data(&Trigger_Angle_PID);
+				PID_Func.Clear_PID_Data(&Trigger_Speed_PID);
+				
+                niuSystick = SysTick->VAL;
+				if (1)
+				{
+					Shooting->Trigger.Target_Left_Angle = M2006_Trigger[LEFT_TRIGGER_INDEX].Total_Angle + LEFT_TRIGGER_DIRECTION * M2006_ANGLE_1_BULLET;
+					Shooting->Type.Right_Single_Fired_Flag = 1;
+				}
+				else
+				{
+					Shooting->Trigger.Target_Right_Angle = M2006_Trigger[RIGHT_TRIGGER_INDEX].Total_Angle + RIGHT_TRIGGER_DIRECTION * M2006_ANGLE_1_BULLET;
+					Shooting->Type.Right_Single_Fired_Flag = 1;
+				}
+                counter_1++;
+			}
+			// Then fire this
+			else if (Shooting->Type.Right_Single_Fire_Flag && Shooting->Type.Right_Single_Fired_Flag)
+			{
+				/* Left */
+				Shooting->Trigger.Target_Left_Speed = PID_Func.Positional_PID(&Trigger_Angle_PID, Shooting->Trigger.Target_Left_Angle, M2006_Trigger[LEFT_TRIGGER_INDEX].Total_Angle);
+				M2006_Trigger[LEFT_TRIGGER_INDEX].Output_Current = PID_Func.Positional_PID(&Trigger_Speed_PID, Shooting->Trigger.Target_Left_Speed, M2006_Trigger[LEFT_TRIGGER_INDEX].Actual_Speed);
+				/* Right */
+				Shooting->Trigger.Target_Right_Speed = PID_Func.Positional_PID(&Trigger_Angle_PID, Shooting->Trigger.Target_Right_Angle, M2006_Trigger[RIGHT_TRIGGER_INDEX].Total_Angle);
+				M2006_Trigger[RIGHT_TRIGGER_INDEX].Output_Current = PID_Func.Positional_PID(&Trigger_Speed_PID, Shooting->Trigger.Target_Right_Speed, M2006_Trigger[RIGHT_TRIGGER_INDEX].Actual_Speed);
 			}
 
-			else if (Shooting->Type.Right_Burst_Flag)
-			{
-				Shooting->Trigger.Target_Speed = RIGHT_TRIGGER_DIRECTION * 5000; // The multiplying constant determines the frequency of bursting
-				M2006_Trigger[1].Output_Current = PID_Func.Positional_PID(&Trigger_Speed_PID, Shooting->Trigger.Target_Speed, M2006_Trigger[1].Actual_Speed);
-			}
 			else
 			{
-				M2006_Trigger[0].Output_Current = 0;
-				M2006_Trigger[1].Output_Current = 0;
+				M2006_Trigger[LEFT_TRIGGER_INDEX].Output_Current = 0;
+				M2006_Trigger[RIGHT_TRIGGER_INDEX].Output_Current = 0;
 			}
 		}
-	}
-	else
-	{
-		M2006_Trigger[0].Output_Current = 0;
-		M2006_Trigger[1].Output_Current = 0;
 	}
 }
 
